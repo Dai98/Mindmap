cc255f264a0404f110800a39218959ed6c7a146a
dai98 <dai1998555@hotmail.com>
2023-08-13 22:01:35 -0700

    Finish content of week 5

--- Programming-Languages/C++/codes/SJSU-CS205/week5/lab5-1/main.cpp
+++ Programming-Languages/C++/codes/SJSU-CS205/week5/lab5-1/main.cpp
@@ -12,11 +12,18 @@ int main() {
     pf = &ff;
     pd = &dd;
 
+    // The length of pointer is determined by operating system
+    // As of now (2023), most computers are 64-bit, which means pointers on these computers have 64 bits (8 bytes)
+    // char type has 8 bits (1 byte), and its pointer has 8 bytes
     cout << "sizeof(cc) = " << sizeof(cc) << ", sizeof(pc) = " << sizeof(pc) << endl;
+    // int type has 32 bits (4 byte), and its pointer has 8 bytes
     cout << "sizeof(ii) = " << sizeof(ii) << ", sizeof(pi) = " << sizeof(pi) << endl;
+    // float type has 32 bits (4 byte), and its pointer has 8 bytes
     cout << "sizeof(ff) = " << sizeof(ff) << ", sizeof(pf) = " << sizeof(pf) << endl;
+    // double type has 64 bits (8 byte), and its pointer has 8 bytes
     cout << "sizeof(dd) = " << sizeof(dd) << ", sizeof(pd) = " << sizeof(pd) << endl;
 
+    // Pointers are hexadecimal integers, and they have addresses too (&p) 
     cout << "&pc = " << &pc << ", pc = " << static_cast <void*>(pc) << ", *pc = " << *pc << endl;
     cout << "&pi = " << &pi << ", pi = " << pi << ", *pi = " << *pi << endl;
     cout << "&pf = " << &pf << ", pf = " << pf << ", *pf = " << *pf << endl;
@@ -25,6 +32,10 @@ int main() {
     const char *msg = "C/C++ programming is fun.";
     const char *copy;
 
+    // msg is the address of the first element in the char array (C)
+    // when running copy = msg;, copy is also assigned with the address of the first element
+    // therefore, copy and msg are two integer variables with same values
+    // But since they are two separate variables, they have different addresses
     copy = msg;
     cout << "msg = " << msg << ",its address is: " << (void*)msg << ", &msg = " << &msg << endl;
     cout << "copy= " << copy << ",its address is: " << (void*)copy << ", &copy= " << &copy << endl;
--- Programming-Languages/C++/codes/SJSU-CS205/week5/lab5-3/main.cpp
+++ Programming-Languages/C++/codes/SJSU-CS205/week5/lab5-3/main.cpp
@@ -2,18 +2,43 @@
 using namespace std;
 
 int main() {
+    /**
+     * a is actually
+     * [[1, 3, 5, 7],
+     *  [9, 11, 13, 15],
+     *  [17, 19, 0, 0]]
+    */
     int a[][4]={1,3,5,7,9,11,13,15,17,19}; 
+    // Add/Substraction on pointer of 2-dim array will move on the first dimension
+    // a+1 is the pointer to the second row/subarray in a ([9, 11, 13, 15])
+    // *(a+1) gets the actual array value with the pointer
+    // And since an array itself can be used as a pointer, it can be used to assign a pointer variable
+    // Right now p points to the first element in the second subarray of a (namely 9)
     int *p=*(a+1);
+    // Now p points to the fourth element in the second subarray of a (15)
     p += 3;
+    // Here it retrieve the value that p points to (15) and do a self-increment
+    // p points to the next element in the array, which goes to the first element in the third subarray (17)
+    // 2-dimension arrays are still stored as 1-dimension array in memory
+    // So pointers can move across the second dimension
     cout << "*p++ = " << *p++ << ",*p = " << *p << endl; 
 
+    // pc is a pointer on a constant string
     const char *pc = "Welcome to programming.", *r;
     long *q = (long *)pc;
-    q++;
+    // Since long is at least 4-byte long, and char is 1-byte long
+    // So when q does self-increment, it moves 4 bytes forward
+    q++;           
+    // When it is converted back to char pointer
+    // 4 characters are skipped from the start
     r = (char *)q;
     cout << r << endl;
 
+    // m is an int, which is 4 byte long
     unsigned int m = 0x3E56AF67;
+    // short is 2 byte long
+    // So when the pointer to m is converted to short, only the smaller half is saved
+    // Since my system is little endian
     unsigned short *pm = (unsigned short *) &m;
     cout << "*pm = " << hex << *pm << endl;
     
