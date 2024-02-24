90d4235ec9cf51fbb6dde3c34ed1637796635dd6
dai98 <dai1998555@hotmail.com>
2024-02-23 20:21:10 -0800

    Add codes for Min Stack

--- Computer Science/Data Structure&Algorithm/codes/src/data_structure/stack.py
+++ Computer Science/Data Structure&Algorithm/codes/src/data_structure/stack.py
@@ -46,6 +46,9 @@ class Stack(StackTemplate):
         self.size += 1
         self.index += 1
 
+    def peek(self) -> None:
+        return self.data[self.size]
+
 
 class QueueStack(StackTemplate):
     """
@@ -67,8 +70,41 @@ class QueueStack(StackTemplate):
     def pop(self) -> int:
         return self.queue.pop(0)
 
-    def top(self) -> int:
+    def peek(self) -> int:
         return self.queue[0]
 
     def empty(self) -> bool:
         return len(self.queue) == 0
+    
+
+class MinStack(StackTemplate):
+    """
+        Implement the ability to get the minimum value based on basic stack
+        https://leetcode.com/problems/min-stack/description/
+    """
+    def __init__(self) -> None:
+        super().__init__()
+        self.data = []
+        self.min_value = []
+
+    def pop(self) -> Any:
+        element = self.data.pop()
+        self.min_value.pop()
+        return element
+        
+    def push(self, element: Any) -> None:
+        if len(self.data) == 0 and len(self.min_value) == 0:
+            self.data.append(element)
+            self.min_value.append(element)
+        elif self.min_value[-1] <= element:
+            self.data.append(element)
+            self.min_value.append(self.min_value[-1])
+        else:
+            self.data.append(element)
+            self.min_value.append(element)
+    
+    def peek(self):
+        return self.data[-1]
+    
+    def get_min(self):
+        return self.min_value[-1]
