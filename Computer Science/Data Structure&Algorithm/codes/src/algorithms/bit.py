b889102af222740a11191004fc42a4304f9c6cfc
dai98 <dai1998555@hotmail.com>
2024-09-24 19:46:51 -0700

    Add codes and notes of bit manipulation tricks

--- Computer Science/Data Structure&Algorithm/codes/src/algorithms/bit.py
+++ Computer Science/Data Structure&Algorithm/codes/src/algorithms/bit.py
@@ -57,7 +57,7 @@ class Bit:
         Result integer after logical right shift
     """
     @staticmethod()
-    def right_logical_shift(num: int, n: int) -> int:
+    def logical_right_shift(num: int, n: int) -> int:
         """
         If forget about higher bits, num is 1(31bits) for negative or 0(31bits) for positive
         The modulus of 2^32 will get the 31 bits of the number, which is the unsigned part
@@ -76,7 +76,7 @@ class Bit:
     @staticmethod()
     def sign(num: int) -> int:
         # Move sign bit to the LSB
-        return Bit.right_logical_shift(num, 31)
+        return Bit.logical_right_shift(num, 31)
     
     """
     Get the larger number without comparison, Implementation 1
@@ -281,3 +281,218 @@ class Bit:
             ans = ans - (1 << 32)
         return ans
     
+    """
+    Find the minimum larger power of 2 of a given 32-bit integer num
+    E.g. 13 => 16, 30 => 32, 61 => 64, etc.
+
+    When num <= 0, then result is 1
+    When num > 0, then it will contain at least 1 non-zero bit in binary form
+    Then the larger minimum power of 2 is a number that is 1 on the left side of leftmost non-zero bit
+    For example, 13 = 01101, 16 = 10000
+
+    Find the leftmost 1 bit, and then set all bits on its right side to be 1, and then add 1 to get the minimum larger power of 2
+
+    Args:
+        num (int) - A given 32-bit number
+    Return:
+        The minimum larger power of 2
+    """
+    @staticmethod()
+    def find_minimum_larger_power_of_two(num: int):
+        if num <= 0:
+            return 1
+        
+        # Make sure the given number is 32-bit
+        # Because Python is arbitrary precision
+        num = num & 0xFFFFFFFF
+        
+        """
+        E.g. 153 = 010011001
+        num - 1 = 010010000
+        """
+        # Handles when num is already a power of 2
+        num -= 1
+        """
+        010010000 |
+        001001000 = 011011000
+        """
+        num = num | Bit.logical_right_shift(num, 1)
+        """
+        011011000 |
+        000110110 = 011111110
+        """
+        num = num | Bit.logical_right_shift(num, 2)
+        """
+        011111110 |
+        000001111 = 011111111
+        """
+        num = num | Bit.logical_right_shift(num, 4)
+        num = num | Bit.logical_right_shift(num, 8)
+        num = num | Bit.logical_right_shift(num, 16)
+
+        # num + 1 will become 100000000
+        return num + 1
+    
+    """
+    Given an inclusive range [left, right], calculate the bitwise and of all numbers between this range (Leetcode 201)
+
+    For bitwise and, each bit must be the same to be 1, otherwise the result will be 0
+    Depending on the size of the range, for a number n, we need to determine if range is large enough to hold n-1
+    For the rightmost non-zero bit in n, the same bit in n-1 is 0, so this bit will not be kept in the range
+
+    E.g. [n-2, n]
+
+    n-2    011001001100
+    n-1    011001001101
+    n      011001001110
+    -------------------
+    result 011001001100
+
+    The right most bit of n is 0 in n-1, but since the range only has 3 numbers, the bits on the left side are kept in result (0110010011)
+    
+    We don't need to reduce range by 1 every time. We can reduce the range by (right & -right) to reduce the amount of rightmost 1 every time
+    If the range is large enough to hold right - (right & -right), then we can consider this bit as 0, 
+    because during bitwise and process, it will have 0 in all the numbers of this range
+
+
+    Args:
+        left (int) - Left boundary of the range
+        right (int) - Right boundary of the range
+    Return:
+        Bitwise and of all numbers in this range
+    """
+    @staticmethod()
+    def range_bitwise_and(left: int, right: int) -> int:
+        # Reduce range to see which bits to keep during range bitwise
+        # The kept bits are kept because the range is not large enough
+        while left < right:
+            # Brian Kernighan's algorithm to find rightmost non-zero bit to reduce range
+            right -= right & -right
+        return right
+    
+    """
+    Reverse all the bits in the binary form of a given integer (Leetcode 190)
+    Easy Solution - Allocate an array with 32 number, then reverse the bits by setting bits 1 by 1 in the array
+        (How to get each bit - (n >>> i) & 1, i = 0...31)
+
+    E.g. 01101100 => 00110110
+
+    Denote the bits in a binary number as abcdefgh, then the desired result should be hgfedcba
+    Step 1 - Exchange 1 by 1 - ab cd ef gh => ba dc fe hg
+    Step 2 - Exchange 2 by 2 - ba dc fe hg => dc ba hg fe
+    Step 3 - Exchange 4 by 4 - dc ba hg fe => hg fe dc ba => hgfedcba
+    For larger numbers, continue to conduct Exchange 8 by 8 and 16 by 16 as Step 4 and Step 5
+
+    Example Step 1- 
+                a b c d e f g h                        a b c d e f g h
+            &   1 0 1 0 1 0 1 0                    &   0 1 0 1 0 1 0 1
+            -------------------                    -------------------
+                a 0 c 0 e 0 g 0                        0 b 0 d 0 f 0 h
+                
+                0 b 0 d 0 f 0 h <<  1 =     b 0 d 0 f 0 h 0
+                a 0 c 0 e 0 g 0 >>> 1 =   | 0 a 0 c 0 e 0 g
+                                            ---------------
+                                            b a d c f e h g 
+        In Hexadecimal, each 4 bit becomes 1 number, in this case 1x2^1 + 1x2^3 = 10, which is a in hexadecimal
+        Therefore, on left hand side, n is doing bitwise and with 0xaaaaaaaa
+
+        Similarly, 0101 is 1x2^0 + 1x2^2 = 5, therefore on right hand side, n is doing bitwise and with 0x55555555
+    
+        The rest of the steps are similar, each time with different masks on both side and convert them to hexadecimal respectively
+
+    Masks -
+        Step 2 - 1 1 0 0 1 1 0 0   and  0 0 1 1 0 0 1 1
+        Step 3 - 1 1 1 1 0 0 0 0   and  0 0 0 0 1 1 1 1
+        Step 4 - 8 consecutive 1s 8 consecutive 0s 8 consecutive 1s consecutive 0s
+                and consecutive 0s consecutive 1s 8 consecutive 0s consecutive 1s
+        Step 5 - 16 consecutive 1s 16 consecutive 0s
+                and 16 consecutive 0s 16 consecutive 1s
+
+    For last step, we just need to shift the number with 0, then do bitwise or to switch pieces
+
+    Args:
+        n (int) - A given number to reverse bits
+    Return:
+        The integer after reversing bits
+    """
+    @staticmethod()
+    def reverse_bits(n: int) -> int:
+        # Step 1
+        n = (Bit.logical_right_shift((n & 0xaaaaaaaa), 1) | (n & 0x55555555) << 1)
+        # Step 2
+        n = (Bit.logical_right_shift((n & 0xcccccccc), 2) | (n & 0x33333333) << 2)
+        # Step 3
+        n = (Bit.logical_right_shift((n & 0xf0f0f0f0), 4) | (n & 0x0f0f0f0f) << 4)
+        # Step 4
+        n = (Bit.logical_right_shift((n & 0xff00ff00), 8) | (n & 0x00ff00ff) << 8)
+        # Step 5
+        n = (Bit.logical_right_shift(n, 16) | (n << 16))
+        # Restrict output to be 32-bit only, because Python is arbitrary precision
+        n = n & 0xFFFFFFFF
+        return n
+    
+    """
+    Count how many ones are there in a binary form of a given integer, E.g. 01101101 => 5
+    Easy Solution - Go through 32 bits one by one, then get the number of 1s by native counting
+        (How to get each bit - (n >>> i) & 1, i = 0...31)
+
+    Use example of 01101101, if consider length 1, each bit is the number of 1 in a piece of length 1
+    Namely, 0 1 1 0 1 1 0 1
+            - - - - - - - -
+            0 1 1 0 1 1 0 1 
+        Step 1 - Get number of 1 in each piece with length 2
+            We can get the number of 1 in piece with length 2 by adding the adjacent pieces with length 1
+            In order to simulate the process of addition, we need to shift the bits
+
+                0 1 1 0 1 1 0 1     0 1 1 0 1 1 0 1 >>> 1 = 0 0 1 1 0 1 1 0
+            &   0 1 0 1 0 1 0 1                         &   0 1 0 1 0 1 0 1
+            -------------------                         -------------------
+                0 1 0 0 0 1 0 1                             0 0 0 1 0 1 0 0
+                (The number of 1                            (The number of 1
+                on right side)                            on left side shifted)
+
+            Simulation of addition
+                0 1 0 0 0 1 0 1
+            +   0 0 0 1 0 1 0 0
+            -------------------
+                0 1 0 1 1 0 0 1
+
+            0 1 1 0 1 1 0 1
+            --- --- --- ---
+            0 1 0 1 1 0 0 1 => The result of addition contains how many 1s in pieces of length 2
+            (1) (1) (2) (1)
+        
+        Then repeat this process to count how many 1s on pieces with length 8, 16 and 32
+
+    Args:
+        n (int) - A given number to count how many 1s in the binary form
+    Return:
+        The number of 1 in the binary form of given number
+    """
+    def count_ones(self, n: int) -> int:
+        # n is how many 1s in pieces of length 2
+        n = (n & 0x55555555) + (Bit.logical_right_shift(n, 1) & 0x55555555)
+        # n is how many 1s in pieces of length 4
+        n = (n & 0x33333333) + (Bit.logical_right_shift(n, 2) & 0x33333333)
+        # n is how many 1s in pieces of length 8
+        n = (n & 0x0f0f0f0f) + (Bit.logical_right_shift(n, 4) & 0x0f0f0f0f)
+        # n is how many 1s in pieces of length 16
+        n = (n & 0x00ff00ff) + (Bit.logical_right_shift(n, 8) & 0x00ff00ff)
+        # n is how many 1s in pieces of length 32
+        n = (n & 0x0000ffff) + (Bit.logical_right_shift(n, 16) & 0x0000ffff)
+
+        # The number of 1s on piece of length 32 is exactly the number of 1s in the binary form of the given number
+        return n
+    
+    """
+    Calculating the Hamming Distance of two given integers (Leetcode 461)
+    Hamming distance is how many different bits are there in the binary form of two integers
+
+    Args:
+        x (int) - A number to calculate hamming distance
+        y (int) - A number to calculate hamming distance
+    Return:
+        The hamming distance of two given integers
+    """
+    def hamming_distance(self, x: int, y: int) -> int:
+        return self.count_ones(x ^ y)
