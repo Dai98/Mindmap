6de66f8424cc473b02965f74f4471bf3c465fbbb
dai98 <dai1998555@hotmail.com>
2024-10-17 22:10:51 -0700

    Add notes and codes of Props in Module 2

--- Web Development/React.js/codes/udemy-react/react-project-02/src/App.jsx
+++ Web Development/React.js/codes/udemy-react/react-project-02/src/App.jsx
@@ -1,23 +1,31 @@
-import reactImg from './assets/react-core-concepts.png';
-
-function Header() {
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
+import Header from './components/Header/Header';
+import CoreConcept from './components/CoreConcept/CoreConcept'
+import TabButton from './components/TabButton/TabButton';
+import { CORE_CONCEPTS } from './data'
 
 function App() {
   return (
     <div>
       <Header />
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]}/>
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/>
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
